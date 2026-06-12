5b60e79b19afd3e9ef20881603027d1733153f11
Ксения Котельникова <xeniia.ka@gmail.com>
2026-06-12 20:36:12 +0300

    cleanup: delete notes from State + last bits
    Signed-off-by: Ксения Котельникова <xeniia.ka@gmail.com>

--- src/State.v
+++ src/State.v
@@ -8,28 +8,22 @@ Require Export Id.
 
 Section S.
 
-  (* Set means that type is arbitrary *)
   Variable A : Set.
   
-  (* list of pairs: id with type A *)
   Definition state := list (id * A). 
 
   Reserved Notation "st / x => y" (at level 0).
 
   Inductive st_binds : state -> id -> A -> Prop := 
-  (* if (id, x) is in the head pair, return x *)
     st_binds_hd : forall st id x, ((id, x) :: st) / id => x
-  (* if fst pair isnt id, then exclude it from st and try again *)
   | st_binds_tl : forall st id x id' x', id <> id' -> st / id => x -> ((id', x')::st) / id => x
   where "st / x => y" := (st_binds st x y).
 
-  (* put new pair inro list (it will override (shadow?) old value, if exists, because st_binds takes first pair) *)
   Definition update (st : state) (id : id) (a : A) : state := (id, a) :: st.
 
   Notation "st [ x '<-' y ]" := (update st x y) (at level 0).
   
   (* Functional version of binding-in-a-state relation *)
-  (* like st_binds, finds the value of given id in given state *)
   Fixpoint st_eval (st : state) (x : id) : option A :=
     match st with
     | (x', a) :: st' =>
@@ -40,15 +34,45 @@ Section S.
   (* State a prove a lemma which claims that st_eval and
      st_binds are actually define the same relation.
   *)
-  (* TODO: Lemma st_binds_eval_eq : forall st x a, st / x => a <-> st_eval st x = Some a.
+  (* helper for st_eval_iff_st_binds *)
+  Lemma st_eval_to_st_binds : forall (st : state) (x : id) (a : A),
+    st_eval st x = Some a -> st / x => a.
   Proof.
-    split.
-    - (* => *)
+    intros st; induction st as [| [x' a'] st' IHst].
+    - (* [] *) intros x a H; simpl; discriminate H.
+    - (* :: *)
+      intros x a H; simpl in H; destruct (id_eq_dec x' x) as [H_eq | H_neq].
+      + (* = *)
+        subst x'. injection H as H_eq_a. subst a'.
+        apply st_binds_hd.
+      + (* <> *)
+        apply st_binds_tl.
+        * congruence. 
+        * apply IHst. exact H.
+  Qed.
 
-    - (* <= *)
-  Qed.*)
+  (* helper for st_eval_iff_st_binds *)
+  Lemma st_binds_to_st_eval : forall (st : state) (x : id) (a : A),
+    st / x => a -> st_eval st x = Some a.
+  Proof.
+    intros st x a H; induction H as [st' id y | st' id y id' x' H_neq H_binds IHst].
+    - (* hd *) simpl; destruct (id_eq_dec id id) as [H_eq | H_neq].
+      + reflexivity.
+      + contradiction H_neq. reflexivity.
+    - (* tl *) simpl; destruct (id_eq_dec id' id) as [H_eq' | H_neq'].
+      + subst id'. contradiction H_neq. reflexivity.
+      + exact IHst.
+  Qed.
 
-  (* if state binds x to both n and m, then n=m *)
+  Lemma st_eval_iff_st_binds : forall (st : state) (x : id) (a : A),
+    st_eval st x = Some a <-> st / x => a.
+  Proof.
+    intros st x a. split.
+    - apply st_eval_to_st_binds.
+    - apply st_binds_to_st_eval.
+  Qed.
+  
+
   Lemma state_deterministic' (st : state) (x : id) (n m : option A)
     (SN : st_eval st x = n)
     (SM : st_eval st x = m) :
@@ -57,7 +81,6 @@ Section S.
     subst n. subst m. reflexivity.
   Qed.
   
-  (* same for st_binds *)
   Lemma state_deterministic (st : state) (x : id) (n m : A)   
     (SN : st / x => n)
     (SM : st / x => m) :
@@ -66,19 +89,17 @@ Section S.
     induction SN;
     inversion SM;
     subst;
-    auto; (* half of cases: hd/hd and tl/tl, will succeed, other half will cause a conrtadiction *)
+    auto;
     contradiction.
   Qed.
   
-  (* update works *)
   Lemma update_eq (st : state) (x : id) (n : A) :
     st [x <- n] / x => n.
   Proof.
-    unfold update. (* places actual function into expression *)
+    unfold update.
     constructor.
   Qed.
 
-  (* updating one value doesnt change the other *)
   Lemma update_neq (st : state) (x2 x1 : id) (n m : A)
         (NEQ : x2 <> x1) : st / x1 => m <-> st [x2 <- n] / x1 => m.
   Proof.
@@ -91,7 +112,6 @@ Section S.
       intros H; inversion H; subst; [contradiction | assumption].
   Qed.
   
-  (* shadowing of one var doesnt affect other. TODO: check if could be more elegant *)
   Lemma update_shadow (st : state) (x1 x2 : id) (n1 n2 m : A) :
     st[x2 <- n1][x2 <- n2] / x1 => m <-> st[x2 <- n2] / x1 => m.
   Proof.
@@ -120,7 +140,6 @@ Section S.
         apply update_neq; auto.
   Qed.
   
-  (* update with same value doesnt affect other pairs *)
   Lemma update_same (st : state) (x1 x2 : id) (n1 m : A)
         (SN : st / x1 => n1)
         (SM : st / x2 => m) :
@@ -135,16 +154,8 @@ Section S.
     - apply update_neq.
       + exact H_NotEq.
       + exact SM.
-    (*intros.
-    unfold update.
-    induction SN.
-    induction SM.
-    assert (m = n1) by (eapply state_deterministic; eauto).
-    subst.
-    apply update_eq.*)
   Qed.
   
-  (* the order of updates doesnt affect other pairs. TODO: more elegant? *)
   Lemma update_permute (st : state) (x1 x2 x3 : id) (n1 n2 m : A)
         (NEQ : x2 <> x1)
         (SM : st [x2 <- n1][x1 <- n2] / x3 => m) :
@@ -178,9 +189,24 @@ Section S.
       apply update_neq; auto.
   Qed.
 
-  (* not true. TODO: proof *)
+  (* not true. proof below *)
   Lemma state_extensional_equivalence (st st' : state) (H: forall x z, st / x => z <-> st' / x => z) : st = st'.
   Proof. Abort.
+
+  Theorem state_extensional_equivalence_is_wrong :
+    (exists a : A, a = a) -> 
+    (exists st st' : state, (forall x z, st / x => z <-> st' / x => z) /\ st <> st').
+  Proof.
+    intros [a _].
+    exists [(Id 1, a)]; exists [(Id 1, a); (Id 1, a)]; split.
+    - intros x z; rewrite <- st_eval_iff_st_binds; rewrite <- st_eval_iff_st_binds; simpl.
+      destruct (id_eq_dec (Id 1) x) as [H_eq | H_neq].
+      + (* = *)
+        reflexivity.
+      + (* != *)
+        reflexivity.
+    - intros H_false; discriminate H_false.
+  Qed.
 
   Definition state_equivalence (st st' : state) := forall x a, st / x => a <-> st' / x => a.
 
